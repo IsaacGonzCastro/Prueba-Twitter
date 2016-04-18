c3d5c85e38663a01d05768630bea27c170658d69
igonzalezcastro <igonzalezcastro@atanasoff21.danielcastelao.org>
2016-04-18 08:58:33 +0200

    commit Twitter

---
+++ src/pruebatwitter4j/PruebaTwitter4j.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebatwitter4j;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
+/**
+ *
+ * @author igonzalezcastro
+ */
+public class PruebaTwitter4j {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Twitter mitwitter = new TwitterFactory().getInstance();
+
+    }
+
+}
